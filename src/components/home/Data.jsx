a5cad77808c9269e43499c2f61a39ce361ac15a2
James Nguyen <jamesnguyen9611@gmail.com>
2024-08-05 17:31:56 -0500

    Remove home last name

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -4,7 +4,7 @@ const Data = () => {
   return (
     <div className="home__data">
         <h1 className="home__title">
-            James Nguyen
+            James
             <svg
                 width="20"
                 height="20"
