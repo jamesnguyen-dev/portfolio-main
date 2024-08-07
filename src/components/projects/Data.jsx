0179d848ece9d87660a53161eba0a59b9516dd94
James Nguyen <jamesnguyen9611@gmail.com>
2024-08-06 19:09:13 -0500

    Add description and links to projects/data.jsx

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -9,18 +9,24 @@ export const projectsData = [
         image: Project1,
         title: "Project 1",
         category: "A",
+        description: "This is a description of project 1",
+        link: "https://www.example.com/project1",
     },
     {
         id: 2,
         image: Project2,
         title: "Project 2",
         category: "B",
+        description: "This is a description of project 2",
+        link: "https://www.example.com/project2",
     },
     {
         id: 3,
         image: Project3,
         title: "Project 3",
         category: "C",
+        description: "This is a description of project 3",
+        link: "https://www.example.com/project3",
     },
 ];
 
